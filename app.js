2a47aa75b5ddedd61c9e487351177414437c771c
Christian Vega-Munguia <christianvega.cvm@gmail.com>
2021-03-28 15:20:54 -0500

    added cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var headerRouter = require('./routes/header');
@@ -9,6 +10,7 @@ var headerRouter = require('./routes/header');
 var app = express();
 
 app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
