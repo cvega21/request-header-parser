90b00bf0b5b0bed41cb3cbdaf1b0065af997c989
Christian Vega-Munguia <christianvega.cvm@gmail.com>
2021-03-28 15:23:50 -0500

    fixed cors

--- routes/header.js
+++ routes/header.js
@@ -3,11 +3,6 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  console.log(req.headers);
-  console.log(req.headers['accept-language']);
-  console.log(req.headers['user-agent']);
-  console.log(req.headers);
-  console.log(req.ip)
   res.send({"ipaddress": req.ip, "language": req.headers['accept-language'], "software": req.headers['user-agent']});
 });
 
